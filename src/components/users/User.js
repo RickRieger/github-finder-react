756edef274b7746a4370bb88f2274c1b9430697a
Rick Rieger <rickrieger36251@gmail.com>
2021-11-01 06:41:58 -0400

    added mor ui

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -16,6 +16,7 @@ export class User extends Component {
   render() {
     const {
       name,
+      company,
       avatar_url,
       location,
       bio,
@@ -43,6 +44,61 @@ export class User extends Component {
         ) : (
           <i className='fas fa-times-circle text-danger'></i>
         )}
+        <div className='card grid-2'>
+          <div className='all-center'>
+            <img
+              src={avatar_url}
+              className='round-img'
+              style={{ width: '150px' }}
+              alt=''
+            />
+            <h1>{name}</h1>
+            <p>location:{location}</p>
+          </div>
+          <div>
+            {bio && (
+              <Fragment>
+                <h3>Bio</h3>
+                <p>{bio}</p>
+              </Fragment>
+            )}
+            <a href={html_url} className='btn btn_dark my-1'>
+              Visit Github Profile
+            </a>
+            <ul>
+              <li>
+                {login && (
+                  <Fragment>
+                    <strong>Username: </strong>
+                    {login}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {company && (
+                  <Fragment>
+                    <strong>Company: </strong>
+                    {company}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {blog && (
+                  <Fragment>
+                    <strong>Website: </strong>
+                    {blog}
+                  </Fragment>
+                )}
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className='card text-center'>
+          <div className='badge badge-primary'>Followers:{followers}</div>
+          <div className='badge badge-success'>Following:{following}</div>
+          <div className='badge badge-danger'>Public Repos:{public_repos}</div>
+          <div className='badge badge-dark'>Public Gists:{public_gists}</div>
+        </div>
       </Fragment>
     );
   }
